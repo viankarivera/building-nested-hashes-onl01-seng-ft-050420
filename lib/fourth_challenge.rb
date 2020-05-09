da616ecfae98e94ced9690c339091c0e19478d59
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 14:13:09 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,10 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        {name: "Benvolio", age: "17", attitude: "worried"}
+        {name: "Mercutio" age: "18", attitude: "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -18,6 +19,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
