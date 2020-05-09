45a0c1dfb671523a1dedb0e5726068fdfc9bcc0b
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 14:03:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,24 +2,24 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: => "Lord Montague"
-        age: => "53"
+        name: "Lord Montague"
+        age: "53"
         },
       :matriarch => {
-        name: => "Lady Montague"
-        age: => "54"
+        name: "Lady Montague"
+        age: "54"
         },
       :hero => {
-        name: => "Romeo"
-        age: => "15"
-        status: => "alive"
+        name: "Romeo"
+        age: "15"
+        status: "alive"
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet"
-        :age => "50"
+        name: "Lord Capulet"
+        age: "50"
         },
       :matriarch => {
         :name => "Lady Capulet"
